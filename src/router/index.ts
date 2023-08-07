1193ce0c691ebfc252a7d5e91a1bbdcaf24de798
dianachristine <christine.tarro@gmail.com>
2023-08-07 20:02:48 +0300

    Try to fix routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -32,7 +32,8 @@ console.log("url")
 console.log(import.meta.env.BASE_URL);
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  //history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
